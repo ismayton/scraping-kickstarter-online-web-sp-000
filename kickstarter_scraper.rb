fdbe2742e5f2bb0d14ee0b4e5a2ca350c159fe03
Ian Mayton <ian.mayton@gmail.com>
2020-07-30 16:33:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
-  # projects:
   #project = kickstarter.css("li.project.grid_4")
+  #title = 
   binding.pry
 end
 
